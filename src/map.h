4dd97fcb0bf95ca16b4e252e1226a6881129cb4c
Thundzz <yacinenew@gmail.com>
2013-12-19 14:10:30 +0100

    Ajouté une implémentation de map

---
+++ src/map.h
@@ -0,0 +1,20 @@
+#ifndef MAP_H
+#define MAP_H
+
+/* Constants Declaration */
+
+#define MAX_VAR 1000
+
+/* Function prototypes */
+
+void init() ;
+
+void end();
+
+int get_val(char * variable);
+
+void set_val(char * variable, int value);
+
+#endif
+
+
